35a5ec64bcd2a96afb126f840264df1a3fc8c3ac
wesley <wesley.rochao@hotmail.com>
2024-10-11 16:35:09 -0300

    t

---
+++ Proa_react/aula01/src/assets/components/Icons.jsx
@@ -0,0 +1,10 @@
+import {} from 'react-icons'
+
+function Icons({icone}){
+    return(
+        <span className='sidebar-icon'>
+            {icone}
+        </span>
+    )
+}
+export default Icons
--- Proa_react/aula01/src/assets/components/Sidebar.jsx
+++ Proa_react/aula01/src/assets/components/Sidebar.jsx
@@ -1,10 +1,14 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+import Icons from "./Icons";
+
 function Sidebar(){
     return(
-        <div className="fixed top-0 flex flex-col h-screen bg-gray-900 w-16">
-            <span>A</span>
-            <span>B</span>
-            <span>C</span>
-            <span>D</span>
+        <div className="fixed top-0 flex items-center flex-col h-screen bg-gray-900 w-16 text-white">
+            <Icons icone={<BsGearFill size={32}/>}/>
+            <Icons icone={<FaPoo size={32}/>}/>
+            <Icons icone={<BsGearFill size={32}/>}/>
+            <Icons icone={<BsGearFill size={32}/>}/>
         </div>
     )
 }
